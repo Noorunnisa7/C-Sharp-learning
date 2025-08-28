bba20071d14b2d137be731fbeda82eb12d7a361f
noorunnisa7 <noorunnisa560@gmail.com>
2025-08-28 05:45:07 -0700

    Abstraction and Interfaces

---
+++ Class12/Class12/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class12
+{
+    abstract class Animal
+    {
+        public abstract void Sound();
+        public abstract void COlor();
+        public void Detail()
+        {
+            Console.WriteLine("Abstract Class and method");
+            Console.WriteLine("This is Animal Class");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("The Cat Meows");
+        }
+        public override void COlor()
+        {
+            Console.WriteLine("Cat Color is Black");
+        }
+    }
+    class Horse : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("The Horse ....");
+        }
+        public override void COlor()
+        {
+            Console.WriteLine("Horse Color is Brown");
+        }
+    }
+}
---
+++ Class12/Class12/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class12
+{
+    class Program
+    {
+        interface IStudent
+        {
+            void studentDetails();
+            void Courses();
+        }
+
+        class Student1 : IStudent
+        {
+            public void studentDetails()
+            {
+                Console.WriteLine("Name: Ali \n Age: 24");
+            }
+            public void Courses()
+            {
+                Console.WriteLine("Course: C# ");
+            }
+        }
+        
+
+
+
+
+        static void Main(string[] args)
+        {
+            //Animal a = new Animal();
+            //a.Detail();
+
+            Cat a = new Cat();
+            a.Sound();
+
+            Animal b = new Cat();
+            b.Sound();
+            b.COlor();
+
+            Animal c = new Horse();
+            c.Sound();
+            c.COlor();
+
+            IStudent std1 = new Student1();
+            std1.studentDetails();
+            std1.Courses();
+
+            Console.ReadKey();
+        }
+    }
+}
